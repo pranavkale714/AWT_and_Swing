2eb4fd80f125a6cd3adde5163f294040ccfae342
pranavkale714 <96164632+pranavkale714@users.noreply.github.com>
2025-04-01 23:37:19 +0530

    Add files via upload

---
+++ Theory_1.java
@@ -0,0 +1,12 @@
+package AWT_Abstract_window_toolkit;
+
+public class Theory_1 {
+
+	public static void main(String[] args) {
+		System.out.println("Java AWT (Abstract Window Toolkit) is an API to develop Graphical User Interface (GUI) or windows-based applications in Java.");
+		System.out.println("Java AWT components are platform-dependent i.e. components are displayed according to the view of operating system");
+		System.out.println("AWT is heavy weight i.e. its components are using the resources of underlying operating system (OS).");
+		System.out.println("The java.awt package provides classes for AWT API such as TextField, Label, TextArea, RadioButton, CheckBox, Choice, List etc.");
+	}
+
+}
